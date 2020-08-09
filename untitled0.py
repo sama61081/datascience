6edd08b08d53d3a11ef9b11445969a125d5db768
Srinivas Reddy Sama <69422953+ssreddy12121@users.noreply.github.com>
2020-08-09 19:57:08 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,8 @@
+class Test:
+    @staticmethod
+    def add():
+        x=int(input("enter first num:"))
+        y=int(input("enter second num:"))
+        print("sum of two nums:",x+y)
+t1=Test()
+t1.add()
